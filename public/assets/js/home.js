e69d34656e4d822a90d9d647cf7b5b6946c5d59d
Jessi Calicotte <orgkleen@gmail.com>
2019-12-11 17:43:54 +0000

    Redirect users from the client using location.replace, Build the start of the boards page, Build the start of the boards page, set element attributes using jQuery, and set to make Ajax requests

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -1,11 +1,18 @@
+const $submitButton = $('#submit');
 const $setLogin = $('#login');
 const $setSignUp = $('#signup');
-const $submitButton = $('#submit');
 const $emailInput = $('#email');
 const $passwordInput = $('#password');
 const $message = $('#message');
 
 let authSetting = 'login';
+
+sendUserToBoards();
+
+function sendUserToBoards() {
+  if (localStorage.getItem('user')) {
+    location.replace('/boards');
+  }
 
 function setAuth(setting) {
   authSetting = setting;
@@ -22,23 +29,77 @@ function setAuth(setting) {
 }
 
 function handleFormSubmit(event) {
-    event.preventDefault();
-    
-    let email = $emailInput.val().trim();
-    let password = $passwordInput.val().trim();
+  event.preventDefault();
 
-    if (!email || !password) {
-        displayMessage('Email and password fields cannot be blank.', 'danger');
-        return;
-      }
+  let email = $emailInput.val().trim();
+  let password = $passwordInput.val().trim();
 
-    console.log(
-      `Email: ${email} Password: ${password} AuthSetting: ${authSetting}`
-    );
+  if (!email || !password) {
+    displayMessage('Email and password fields cannot be blank.', 'danger');
+    return;
   }
-  function displayMessage(message, type) {
+
+  $emailInput.val('');
+  $passwordInput.val('');
+
+  authenticateUser(email, password);
+}
+
+function displayMessage(message, type) {
   $message.text(message).attr('class', type);
 }
-  $setLogin.on('click', setAuth.bind(null, 'login')); 
-  $setSignUp.on('click', setAuth.bind(null, 'signup'));
-  $submitButton.on('click', handleFormSubmit);
+
+function handleSignupResponse(status) {
+  if (status === 'success') {
+    displayMessage('Registered successfully! You may now sign in.', 'success');
+    setAuth('login');
+  } else {
+    displayMessage(
+      'Something went wrong. A user with this account may already exist.',
+      'danger'
+    );
+  }
+}
+function handleLoginResponse(data, status, jqXHR) {
+  if (status === 'success') {
+    let jwt = jqXHR.getResponseHeader('authorization');
+    let user = JSON.stringify(data);
+
+    localStorage.setItem('authorization', jwt);
+    localStorage.setItem('user', user);
+    sendUserToBoards();
+  } else {
+    displayMessage('Invalid email or password.', 'danger');
+  };
+}
+function authenticateUser(email, password) {
+  $.ajax({
+    url: '/' + authSetting,
+    data: {
+      user: {
+        email,
+        password
+      }
+    },
+    method: 'POST'
+  })
+    .then(function(data, status, jqXHR) {
+      if (authSetting === 'signup') {
+        handleSignupResponse(status);
+      } else {
+        handleLoginResponse(data, status, jqXHR);
+      }
+    })
+    .catch(function(err) {
+      if (authSetting === 'signup') {
+        handleSignupResponse(err.statusText);
+      } else {
+        handleLoginResponse(err.statusText);
+      }
+    });
+}
+
+$setLogin.on('click', setAuth.bind(null, 'login'));
+$setSignUp.on('click', setAuth.bind(null, 'signup'));
+$submitButton.on('click', handleFormSubmit);
+ 
